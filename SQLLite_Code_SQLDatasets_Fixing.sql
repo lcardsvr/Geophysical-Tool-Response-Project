c7fd8de8d57ff0e20710c6598220f778eb20d5fc
Luis Cardenas <lcardsvr@gmail.com>
2023-05-30 20:43:38 +0800

    Add new files

---
+++ SQLLite_Code_SQLDatasets_Fixing.sql
@@ -0,0 +1,263 @@
+-- SQLLite Code to fix first SQL Dataset
+
+alter table "0028AA"
+RENAME to Century_0028AA;
+
+alter table Century_0028AA
+add COLUMN ind;
+
+alter table "0028AA_cals"
+RENAME to Century_0028AA_cals;
+
+alter table Century_0028AA_cals
+add COLUMN ind;
+
+alter table "0028AA_header"
+RENAME to Century_0028AA_header;
+
+alter table Century_0028AA_header
+add COLUMN ind;
+
+alter table "9013A"
+RENAME to Century_9013A;
+
+alter table Century_9013A
+add COLUMN ind;
+
+alter table "9013A_cals"
+RENAME to Century_9013A_cals;
+
+alter table Century_9013A_cals
+add COLUMN ind;
+
+alter table "9013A_header"
+RENAME to Century_9013A_header;
+
+alter table Century_9013A_header
+add COLUMN ind;
+
+alter table "9238AA"
+RENAME to Century_9238AA;
+
+alter table Century_9238AA
+add COLUMN ind;
+
+alter table "9238AA_cals"
+RENAME to Century_9238AA_cals;
+
+alter table Century_9238AA_cals
+add COLUMN ind;
+
+alter table "9238AA_header"
+RENAME to Century_9238AA_header;
+
+alter table Century_9238AA_header
+add COLUMN ind;
+
+alter table "9238CA"
+RENAME to Century_9238CA;
+
+alter table Century_9238CA
+add COLUMN ind;
+
+alter table "9238CA_cals"
+RENAME to Century_9238CA_cals;
+
+alter table Century_9238CA_cals
+add COLUMN ind;
+
+alter table "9238CA_header"
+RENAME to Century_9238CA_header;
+
+alter table Century_9238CA_header
+add COLUMN ind;
+
+alter table "9622A"
+RENAME to Century_9622A;
+
+alter table Century_9622A
+add COLUMN ind;
+
+alter table "9622A_cals"
+RENAME to Century_9622A_cals;
+
+alter table Century_9622A_cals
+add COLUMN ind;
+
+alter table "9622A_header"
+RENAME to Century_9622A_header;
+
+alter table Century_9622A_header
+add COLUMN ind;
+
+alter table "9622C"
+RENAME to Century_9622C;
+
+alter table Century_9622C
+add COLUMN ind;
+
+alter table "9622C_cals"
+RENAME to Century_9622C_cals;
+
+alter table Century_9622C_cals
+add COLUMN ind;
+
+alter table "9622C_header"
+RENAME to Century_9622C_header;
+
+alter table Century_9622C_header
+add COLUMN ind;
+
+
+-- SQL Code to fix the second SQLLite Dataset
+
+alter table "0028AA"
+RENAME to Century_0028AA;
+
+alter table Century_0028AA
+add COLUMN ind;
+
+alter table "0028AA_cals"
+RENAME to Century_0028AA_cals;
+
+alter table Century_0028AA_cals
+add COLUMN ind;
+
+alter table "0028AA_header"
+RENAME to Century_0028AA_header;
+
+alter table Century_0028AA_header
+add COLUMN ind;
+
+alter table "0037A"
+RENAME to Century_0037A;
+
+alter table Century_0037A
+add COLUMN ind;
+
+alter table "0037A_cals"
+RENAME to Century_0037A_cals;
+
+alter table Century_0037A_cals
+add COLUMN ind;
+
+alter table "0037A_header"
+RENAME to Century_0037A_header;
+
+alter table Century_0037A_header
+add COLUMN ind;
+
+alter table "0037C"
+RENAME to Century_0037C;
+
+alter table Century_0037C
+add COLUMN ind;
+
+alter table "0037C_cals"
+RENAME to Century_0037C_cals;
+
+alter table Century_0037C_cals
+add COLUMN ind;
+
+
+alter table "0037C_header"
+RENAME to Century_0037C_header;
+
+alter table Century_0037C_header
+add COLUMN ind;
+
+alter table "9622A"
+RENAME to Century_9622A;
+
+alter table Century_9622A
+add COLUMN ind;
+
+alter table "9622A_cals"
+RENAME to Century_9622A_cals;
+
+alter table Century_9622A_cals
+add COLUMN ind;
+
+alter table "9622A_header"
+RENAME to Century_9622A_header;
+
+alter table Century_9622A_header
+add COLUMN ind;
+
+alter table "9622C"
+RENAME to Century_9622C;
+
+alter table Century_9622C
+add COLUMN ind;
+
+alter table "9622C_cals"
+RENAME to Century_9622C_cals;
+
+alter table Century_9622C_cals
+add COLUMN ind;
+
+alter table "9622C_header"
+RENAME to Century_9622C_header;
+
+alter table Century_9622C_header
+add COLUMN ind;
+
+-- SQL Code to fix the third Dataset
+
+
+alter table "0028AA"
+RENAME to Century_0028AA;
+
+alter table Century_0028AA
+add COLUMN ind;
+
+alter table "0028AA_cals"
+RENAME to Century_0028AA_cals;
+
+alter table Century_0028AA_cals
+add COLUMN ind;
+
+alter table "0028AA_header"
+RENAME to Century_0028AA_header;
+
+alter table Century_0028AA_header
+add COLUMN ind;
+
+alter table "9622A"
+RENAME to Century_9622A;
+
+alter table Century_9622A
+add COLUMN ind;
+
+alter table "9622A_cals"
+RENAME to Century_9622A_cals;
+
+alter table Century_9622A_cals
+add COLUMN ind;
+
+alter table "9622A_header"
+RENAME to Century_9622A_header;
+
+alter table Century_9622A_header
+add COLUMN ind;
+
+alter table "9622C"
+RENAME to Century_9622C;
+
+alter table Century_9622C
+add COLUMN ind;
+
+alter table "9622C_cals"
+RENAME to Century_9622C_cals;
+
+alter table Century_9622C_cals
+add COLUMN ind;
+
+alter table "9622C_header"
+RENAME to Century_9622C_header;
+
+alter table Century_9622C_header
+add COLUMN ind;
+
+
+
